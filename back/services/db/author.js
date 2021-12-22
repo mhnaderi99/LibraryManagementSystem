ef30c23510f77a01074bbd14a0ccca4e26aa52fa
popsiqle <popsiqle@hotmail.com>
2021-12-22 16:13:07 +0330

    add author service methods

--- back/services/db/author.js
+++ back/services/db/author.js
@@ -3,6 +3,48 @@ const router = express.Router();
 const db = require('../../config/database');
 const Author = require('../../models/Author');
 
+function findAuthorByName(authorName, page, authorsInPage) {
+    const offset = authorsInPage * (page - 1);
+    const limit = authorsInPage;
+    const Op = Sequelize.Op;
+    return Author.findAll({
+            offset: offset,
+            limit: limit,
+            where: {
+                firstname: {
+                    [Op.like]: '%' + authorName + '%'
+                },
+                lastname: {
+                    [Op.like]: '%' + authorName + '%'
+                }
+            }
+        })
+        .then((foundAuthor) => {
+            return foundAuthor;
+        })
+        .catch(err => {
+            console.log(err);
+            return null;
+        });
+}
+
+function findAuthorByNationality(authorNationality, page, authorsInPage) {
+    const offset = authorsInPage * (page - 1);
+    const limit = authorsInPage;
+    return Author.findAll({
+            offset: offset,
+            limit: limit,
+            where: { nationality: authorNationality }
+        })
+        .then((foundAuthor) => {
+            return foundAuthor;
+        })
+        .catch(err => {
+            console.log(err);
+            return null;
+        });
+}
+
 function getAllAuthors(page, authorsInPage) {
     const offset = authorsInPage * (page - 1);
     const limit = authorsInPage;
@@ -21,7 +63,6 @@ function getAllAuthors(page, authorsInPage) {
         });
 }
 
-
 function createAuthor(newAuthor) {
     return Author.create(newAuthor)
         .then((createdAuthor) => {
@@ -34,7 +75,21 @@ function createAuthor(newAuthor) {
         });
 }
 
+function editAuthor(editedFields, authorId) {
+    return Author.update(editedFields, { where: { id: authorId } })
+        .then((editedAuthor) => {
+            return editedAuthor;
+        })
+        .catch(err => {
+            console.log(err);
+            return null;
+        });
+}
+
 module.exports = {
+    findAuthorByName,
+    findAuthorByNationality,
     getAllAuthors,
-    createAuthor
+    createAuthor,
+    editAuthor
 };
